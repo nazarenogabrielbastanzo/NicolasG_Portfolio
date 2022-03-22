f6534ed67e7c8a740f9047a7c58068126e385244
nicolasggdev <nicolasggdev@gmail.com>
2022-03-22 11:00:22 -0500

    Create styles for about me, add img and I corrected the spelling

--- src/View/About/About.jsx
+++ src/View/About/About.jsx
@@ -1,97 +1,102 @@
 import React from "react";
 
+// Styles
+import "./About.styles.css";
+
+// Utils
+import Academlo from "../../Utils/Img/Academlo.png";
+import Youtube from "../../Utils/Img/Youtube.png";
+import Html from "../../Utils/Img/Html.png";
+import Css from "../../Utils/Img/Css.png";
+import JavaScript from "../../Utils/Img/JavaScript.png";
+import NodeJs from "../../Utils/Img/NodeJs.png";
+import Reacticon from "../../Utils/Img/React.png";
+import Git from "../../Utils/Img/Git.png";
+import Github from "../../Utils/Img/Github.png";
+import PostgresQL from "../../Utils/Img/PostgresQL.png";
+import MongoDB from "../../Utils/Img/MongoDB.png";
+import Netlify from "../../Utils/Img/Netlify.png";
+import Heroku from "../../Utils/Img/Heroku.png";
+
 const About = () => {
   return (
-    <div>
-      <section>
-        <h3>About</h3>
-        <h1>Nicolas Gonzalez Giraldo</h1>
-      </section>
-      <section>
+    <div className="container-about">
+      <section className="section-about">
+        <h3>
+          <b>About me </b>
+        </h3>
         <p>
-          Hey there, I'm Nicolas! First of all, thanks for visiting my personal
-          website
+          📌 Hey there, I'm Nicolas! Thanks for visiting my personal website.
         </p>
       </section>
       <section>
         <article>
-          <h2>Who Am I?</h2>
+          <h2 className="titles">Who Am I?</h2>
           <p>
             I'm a developer from Colombia who is mainly interested in web
             technologies, especially in backend development.
           </p>
-          <br />
           <p>
             Developing my skills as a self-taught person online platforms (like
-            Academlo Bootcamp, Youtube). Currenlly I'am studing in the
-            university in the government program MisionTic 2022
+            <img className="icons" src={Academlo} alt="Academlo-Icon" />
+            Academlo Bootcamp and
+            <img className="icons" src={Youtube} alt="Academlo-Icon" />
+            Youtube). Currenlly I'am studing in the university (UTP) in the
+            colombian government program MisionTic 2022.
           </p>
-          <br />
           <p>
-            My curiosity makes me able to quickly learn new tools and
-            technologies when needed. I enjoy sharing my knowledge and helping
-            others when possible.
+            My curiosity makes me able to learn quickly new tools and
+            technologies when I needed. I enjoy sharing my knowledge and helping
+            others when is possible.
           </p>
-          <br />
-          <h2>My Stack</h2>
+          <h2 className="titles">My Stack</h2>
           <p>These are the technologies I like to use in my projects:</p>
-          <ul>
+          <ul className="ul-tecnologies">
             <li>
-              {/* IMG */}
+              <img className="icons" src={Html} alt="Html-Icon" />
               <span>HTML</span>
             </li>
             <li>
-              {/* IMG */}
+              <img className="icons" src={Css} alt="Css-Icon" />
               <span>CSS</span>
             </li>
             <li>
-              {/* IMG */}
-              <span>Boostrap</span>
-            </li>
-            <li>
-              {/* IMG */}
+              <img className="icons" src={JavaScript} alt="JavaScript-Icon" />
               <span>JavaScript</span>
             </li>
             <li>
-              {/* IMG */}
-              <span>NodeJs (Express)</span>
+              <img className="icons" src={NodeJs} alt="NodeJs-Icon" />
+              <span>NodeJs</span>
             </li>
             <li>
-              {/* IMG */}
+              <img className="icons" src={Reacticon} alt="React-Icon" />
               <span>React</span>
             </li>
             <li>
-              {/* IMG */}
+              <img className="icons" src={Git} alt="Git-Icon" />
               <span>Git</span>
             </li>
             <li>
-              {/* IMG */}
+              <img className="icons" src={Github} alt="Github-Icon" />
               <span>Github</span>
             </li>
             <li>
-              {/* IMG */}
+              <img className="icons" src={PostgresQL} alt="PostgresQL-Icon" />
               <span>PostgresQL</span>
             </li>
             <li>
-              {/* IMG */}
-              <span>Firebase</span>
+              <img className="icons" src={MongoDB} alt="MongoDB-Icon" />
+              <span>MongoDB</span>
             </li>
             <li>
-              {/* IMG */}
+              <img className="icons" src={Netlify} alt="Netlify-Icon" />
               <span>Netlify</span>
             </li>
             <li>
-              {/* IMG */}
+              <img className="icons" src={Heroku} alt="MongoDB-Icon" />
               <span>Heroku</span>
             </li>
           </ul>
-          <h2>Wanna talk?</h2>
-          <p>
-            If you wanna get in touch or talk about a project, feel free to
-            contact you can also send me a DM on
-            <a href="https://www.linkedin.com/in/nicolasggdev/">Linkeind</a> or
-            via to email <b> nicolasggdev@gmail.com</b>.
-          </p>
         </article>
       </section>
     </div>
--- src/View/Contact/Contact.jsx
+++ src/View/Contact/Contact.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div>
+      <h2>Wanna talk?</h2>
+      <p>
+        If you wanna get in touch or talk about a project, feel free to contact
+        you can also send me a DM on
+        <a href="https://www.linkedin.com/in/nicolasggdev/">Linkeind</a> or via
+        to email
+      </p>
+    </div>
+  );
 };
 
 export default Contact;
