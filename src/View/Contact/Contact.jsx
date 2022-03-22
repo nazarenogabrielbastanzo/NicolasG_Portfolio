512d295887edd900fc5bafa5f5e382bf52dd73ec
nicolasggdev <nicolasggdev@gmail.com>
2022-03-22 15:44:04 -0500

    Create a view for Contact with your styles

--- src/View/Contact/Contact.jsx
+++ src/View/Contact/Contact.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 
+// Styles
+import "./Contact.styles.css";
+
 const Contact = () => {
   return (
-    <div>
-      <h2>Wanna talk?</h2>
+    <div className="container-contact">
+      <h2>Contact Me</h2>
       <p>
-        If you wanna get in touch or talk about a project, feel free to contact
-        you can also send me a DM on
-        <a href="https://www.linkedin.com/in/nicolasggdev/">Linkeind</a> or via
-        to email
+        If you want to get in touch, talk about a project or job, feel free to
+        contact you can also send me a message on Linkedin or in to email.
       </p>
     </div>
   );
