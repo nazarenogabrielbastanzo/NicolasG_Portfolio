70490c5a948b1d0986041c9ff4ca9cd21b4daffe
nicolasggdev <nicolasggdev@gmail.com>
2022-03-23 20:19:52 -0500

    I have someones mediaqueries

--- src/View/Contact/Contact.jsx
+++ src/View/Contact/Contact.jsx
@@ -6,8 +6,8 @@ import "./Contact.styles.css";
 const Contact = () => {
   return (
     <div className="container-contact">
-      <h2>Contact Me 💭</h2>
-      <p>
+      <h2 className="title-contact">Contact Me 💭</h2>
+      <p className="p-contact">
         📌If you want to get in touch, talk about a project or job, feel free to
         contact you can also send me a message on Linkedin or in to email.
       </p>
--- src/View/Home/Home.jsx
+++ src/View/Home/Home.jsx
@@ -9,22 +9,20 @@ import "./Home.styles.css";
 const Home = () => {
   return (
     <div className="home">
-      <img
-        className="icon margin-top-home margin-bottom-home"
-        src={blackIcon}
-        alt="Icon"
-      />
-      <h1 className="margin-top-home margin-bottom-home text-center">
+      <img className="icon margin-home" src={blackIcon} alt="Icon" />
+      <h1 className="margin-home text-center title-home">
         Hi, I'm Nicolas Gonzalez G.
       </h1>
-      <div className="margin-top-home margin-bottom-home text-center">
-        <p>
+      <div className="margin-home text-center">
+        <p className="text-home">
           I'm a
           <span className="role">
             <b> Backend Developer</b>
           </span>
         </p>
-        <p>I like create things with web techonologies</p>
+        <p className="text-home">
+          I like to create things with web techonologies
+        </p>
       </div>
     </div>
   );
