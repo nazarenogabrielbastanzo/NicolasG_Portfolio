3548a41b0639bdcc432919aa6b8debe5178370ee
nicolasggdev <nicolasggdev@gmail.com>
2023-03-15 14:30:55 -0500

    Add typing in the banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,14 +1,31 @@
 import React from "react";
+import Typed from "react-typed";
+import Logo from "../../assets/icon/Logo.png";
 import "./Banner.styles.css";
 
 const Banner = () => {
   return (
     <section
-      className="container__banner d-flex align-items-center"
+      className="container__banner d-flex align-items-center p-3"
       id="Banner"
     >
       <div className="w-100 h-75 d-flex align-items-center ">
-        <h1 className="text-center w-100 m-0">Title</h1>
+        <div className="text-center w-100 m-0 animated-type__banner">
+          <img className="logo__banner" src={Logo} alt="nicolasggLogo" />
+          <h1 className="title__banner">
+            <b>Hi! I'm Nicolas GG</b>
+          </h1>
+          <Typed
+            strings={[
+              "I'm a FrontEnd Developer",
+              "I'm a BackEnd Developer",
+              "I'm a FullStack Developer"
+            ]}
+            typeSpeed={150}
+            backSpeed={100}
+            loop
+          ></Typed>
+        </div>
       </div>
     </section>
   );
