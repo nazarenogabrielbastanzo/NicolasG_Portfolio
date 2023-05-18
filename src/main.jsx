7d29df24edcab533f9a656f3950661b2b372ce6a
nicolasggdev <nicolasggdev@gmail.com>
2023-05-17 21:32:34 -0500

    Added vite

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./App.jsx";
 import { Provider } from "react-redux";
 import store from "./store";
 import { I18nextProvider } from "react-i18next";
@@ -21,8 +21,7 @@ i18next.init({
   }
 });
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <I18nextProvider i18n={i18next}>
     <React.StrictMode>
       <Provider store={store}>
